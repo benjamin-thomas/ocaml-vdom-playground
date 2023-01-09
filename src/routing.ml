57293e65bddebcf2e4bb42a69ce02623e8bbb78e
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-09 07:21:56 +0100

    Fake routing WIP

---
+++ src/routing.ml
@@ -0,0 +1,59 @@
+[@@@warning "-32-37"]
+
+module V = Vdom
+
+type page = Home | About
+type model = { curr_page : page }
+type msg = ChangedPage of page
+
+let init = { curr_page = Home }
+
+let update (_model : model) (msg : msg) =
+  match msg with
+  | ChangedPage page -> { curr_page = page }
+;;
+
+let p = V.elt "p"
+let h2 = V.elt "h2"
+let ul = V.elt "ul"
+let li = V.elt "li"
+
+let string_of_page = function
+  | Home -> "Home!"
+  | About -> "About Us"
+;;
+
+let new_page txt page =
+  V.elt "a"
+    [ V.text txt ]
+    ~a:
+      [ V.onclick (fun _ -> ChangedPage page)
+      ; V.attr "href" "javascript:void(0)"
+      ]
+;;
+
+let view model =
+  V.div
+    [ h2 [ V.text "An h2 title" ]
+    ; p
+        [ V.text "A p tag"
+        ; V.div [ p [ V.text @@ string_of_page model.curr_page ] ]
+        ]
+    ; ul
+        [ li [ new_page "Go to home" Home ]
+        ; li [ new_page "Go to about" About ]
+        ]
+    ]
+;;
+
+let app = V.simple_app ~init ~view ~update ()
+
+open Js_browser
+
+let run () =
+  Vdom_blit.run app
+  |> Vdom_blit.dom
+  |> Element.append_child (Document.body document)
+;;
+
+let () = Window.set_onload window run
